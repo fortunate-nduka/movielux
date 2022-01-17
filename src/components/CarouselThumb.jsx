fa076f10ae19526880ac153e900e87f8347ad1ce
Fortunate ogodu <ogodujoseph@gmail.com>
2022-01-17 05:15:28 +0100

    updates

--- src/components/CarouselThumb.jsx
+++ src/components/CarouselThumb.jsx
@@ -4,6 +4,7 @@ import Moment from "react-moment";
 import { AiFillStar } from "react-icons/ai";
 import { FaThumbsUp } from "react-icons/fa";
 import { BsArrowRight } from "react-icons/bs";
+import { ImVideoCamera } from "react-icons/im";
 
 const CarouselThumb = ({
   title,
@@ -32,18 +33,14 @@ const CarouselThumb = ({
       <Header />
       <div className="container py-14">
         <div className="px-4 mb-10">
-          <Moment
-            date={dateToFormat}
-            fromNow
-            className="text-sm text-gray-300"
-          />
+          <Moment date={dateToFormat} fromNow className="text-gray-300" />
           <div className="text-4xl font-bold mt-4">{title || name}</div>
           <div className="flex space-x-4 my-6">
-            <span className="flex items-center text-sm text-gray-300">
+            <span className="flex items-center text-gray-300">
               <AiFillStar className="mr-2 text-[yellow]" />
               {vote_average}
             </span>
-            <span className="flex items-center text-sm text-gray-300">
+            <span className="flex items-center text-gray-300">
               <FaThumbsUp className="mr-2 text-[red]" />
               {vote_count}
             </span>
@@ -54,11 +51,11 @@ const CarouselThumb = ({
               : overview}
           </div>
           <div className="mt-10 flex font-inter flex-col md:flex-row w-max">
-            <button className="text-sm tracking-wider px-7 py-4 bg-[red] rounded-full font-semibold md:mr-6 mb-6 font-inter">
-              Watch Trailer
+            <button className="tracking-wider px-7 py-4 bg-[red] rounded-full font-semibold flex items-center md:mr-6 mb-6">
+              Watch Trailer <ImVideoCamera className="ml-2 text-sm" />
             </button>
-            <button className="px-7 py-4 bg-[white] text-black rounded-full text-sm flex items-center font-inter font-semibold">
-              More Information <BsArrowRight className="ml-2" />
+            <button className="px-7 py-4 bg-[white] text-black rounded-full flex items-center font-semibold">
+              More Information <BsArrowRight className="ml-2 text-sm" />
             </button>
           </div>
         </div>
