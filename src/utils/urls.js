88503b4ac5a25da6d594571dc7ec615c3abd11b1
Fortunate ogodu <ogodujoseph@gmail.com>
2022-01-17 21:20:55 +0100

    updates

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,3 +1,4 @@
 export const imgBase = "https://image.tmdb.org/t/p/original";
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const trendingUrl = `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=em-US&page=1`;
