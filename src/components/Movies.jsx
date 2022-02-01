b3860c4b7fa0b6ae86b7d1049bcb6af9f588e3c1
Fortunate ogodu <ogodujoseph@gmail.com>
2022-02-01 18:46:05 +0100

    update

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -6,13 +6,13 @@ const Movies = () => {
   const { movies } = useContext(DataContext);
   console.log(movies);
   return (
-    <div className="flex items-center justify-center flex-wrap px-5 space-x-7 space-y-5">
+    <div className="flex items-center justify-center flex-wrap gap-10">
       {movies.map((movie) => (
         <div className="flex flex-col justify-start">
           <img
             src={imgBase + movie.poster_path}
             alt={movie.title}
-            className="w-[22rem] sm:w-[15rem] rounded-lg"
+            className="w-[20rem] sm:w-[15rem] rounded-lg"
           />
           hello
         </div>
