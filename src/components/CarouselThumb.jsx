5144ac7586f753fb098f6e0a4a89fa9b046121fc
Fortunate ogodu <ogodujoseph@gmail.com>
2022-02-08 07:48:27 +0100

    update

--- src/components/CarouselThumb.jsx
+++ src/components/CarouselThumb.jsx
@@ -7,7 +7,8 @@ import { FaThumbsUp } from "react-icons/fa";
 import { BsArrowRight } from "react-icons/bs";
 import { BsArrowLeft } from "react-icons/bs";
 import millify from "millify";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
+import overlay from "../assets/bg-noise.gif";
 
 const CarouselThumb = ({
   id,
@@ -57,60 +58,63 @@ const CarouselThumb = ({
   }
 
   return (
-    <div
-      style={style}
-      className="w-full bg-cover bg-center bg-no-repeat min-h-screen md:h-full"
-    >
-      <Header />
-      <div className="container pt-24 pb-20 md:mx-auto md:px-5 relative flex justify-center lg:justify-between items-center">
-        <div className="absolute right-3 top-5">
-          <button className="border border-white rounded-full p-2 mr-3 shadow-lg">
-            <BsArrowLeft fontSize={15} onClick={handlePrev} />
-          </button>
-          <button className="border border-white rounded-full p-2 shadow-lg">
-            <BsArrowRight fontSize={15} onClick={handleNext} />
-          </button>
-        </div>
-        <div className="px-4 mb-10 lg:basis-3/5">
-          <Moment date={dateToFormat} fromNow className="text-gray-300" />
-          <div className="font-montBlack text-3xl md:text-4xl font-bold mt-4 md:mt-5 w-full">
-            {title || name}
-          </div>
-          <div className="flex items-center my-5 md:my-6 lg:my-7">
-            <span className="flex items-center text-gray-300">
-              <AiFillStar className="mr-1 text-[yellow] text-sm md:text-lg" />
-              {vote_average}
-            </span>
-            <div className="inline-block w-1 h-1 bg-gray-300 rounded-full mx-4 md:mx-5"></div>
-            <span className="flex items-center text-gray-300">
-              <FaThumbsUp className="mr-1 text-[#ff3030] text-sm md:text-lg" />
-              {millify(vote_count)}
-            </span>
-            <div className="inline-block w-1 h-1 bg-gray-300 rounded-full mx-4 md:mx-5"></div>
-            <span className="capitalize text-[#ffff00]">{lang}</span>
-          </div>
-          <div className="leading-5 md:leading-7 md:mt-4 mb-10">
-            {overview.length > 250
-              ? `${overview.substring(0, 200)}...`
-              : overview}
-          </div>
-          <div className="flex flex-col md:flex-row justify-center items-center text-center gap-y-7 md:gap-y-0 md:gap-x-5 w-fit">
-          <Link to="/">
-              <button  className="tracking-wider px-7 py-4 bg-[red] rounded-full font-semibold flex items-center cursor-pointer w-fit shadow-lg">
-                More Information <BsArrowRight className="ml-2 text-sm" />
-              </button>
-              </Link>
-            <button className="tracking-wider px-7 py-4 border border-white rounded-full font-semibold flex items-center justify-center ">
-              Watch Trailer <AiOutlineEye className="ml-2 text-lg" />
+  <div className="relative">
+    <div style={{ background: `url(${overlay})` }} className="absolute top-0 left-0 right-0 bottom-0 opacity-[0.06]"></div>
+      <div
+        style={style}
+        className="w-full bg-cover bg-center bg-no-repeat min-h-screen md:h-full"
+      >
+        <Header />
+        <div className="container pt-24 pb-20 md:mx-auto md:px-5 relative flex justify-center lg:justify-between items-center">
+          <div className="absolute right-3 top-5">
+            <button className="border border-white rounded-full p-2 mr-3 shadow-lg">
+              <BsArrowLeft fontSize={15} onClick={handlePrev} />
+            </button>
+            <button className="border border-white rounded-full p-2 shadow-lg">
+              <BsArrowRight fontSize={15} onClick={handleNext} />
             </button>
           </div>
-        </div>
-        <div className="hidden lg:block lg:w-1/4">
-          <img
-            src={`${imgBase + poster_path}`}
-            className="rounded-lg mx-auto shadow-lg w-11/12"
-            alt="movie poster"
-          />
+          <div className="px-4 mb-10 lg:basis-3/5">
+            <Moment date={dateToFormat} fromNow className="text-gray-300" />
+            <div className="font-montBlack text-3xl md:text-4xl font-bold mt-4 md:mt-5 w-full">
+              {title || name}
+            </div>
+            <div className="flex items-center my-5 md:my-6 lg:my-7">
+              <span className="flex items-center text-gray-300">
+                <AiFillStar className="mr-1 text-[yellow] text-sm md:text-lg" />
+                {vote_average}
+              </span>
+              <div className="inline-block w-1 h-1 bg-gray-300 rounded-full mx-4 md:mx-5"></div>
+              <span className="flex items-center text-gray-300">
+                <FaThumbsUp className="mr-1 text-[#ff3030] text-sm md:text-lg" />
+                {millify(vote_count)}
+              </span>
+              <div className="inline-block w-1 h-1 bg-gray-300 rounded-full mx-4 md:mx-5"></div>
+              <span className="capitalize text-[#ffff00]">{lang}</span>
+            </div>
+            <div className="leading-5 md:leading-7 md:mt-4 mb-10">
+              {overview.length > 250
+                ? `${overview.substring(0, 200)}...`
+                : overview}
+            </div>
+            <div className="flex flex-col md:flex-row justify-center items-center text-center gap-y-7 md:gap-y-0 md:gap-x-5 w-fit">
+              <Link to="/">
+                <button className="tracking-wider px-7 py-4 bg-[red] rounded-full font-semibold flex items-center cursor-pointer w-fit shadow-lg">
+                  More Information <BsArrowRight className="ml-2 text-sm" />
+                </button>
+              </Link>
+              <button className="tracking-wider px-7 py-4 border border-white rounded-full font-semibold flex items-center justify-center ">
+                Watch Trailer <AiOutlineEye className="ml-2 text-lg" />
+              </button>
+            </div>
+          </div>
+          <div className="hidden lg:block lg:w-1/4">
+            <img
+              src={`${imgBase + poster_path}`}
+              className="rounded-lg mx-auto shadow-lg w-11/12"
+              alt="movie poster"
+            />
+          </div>
         </div>
       </div>
     </div>
