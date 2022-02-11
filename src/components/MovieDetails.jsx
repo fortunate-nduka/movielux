6f79b1ce415b7005f1d84c3fcf374587cf23fb30
Fortunate ogodu <ogodujoseph@gmail.com>
2022-02-11 23:37:00 +0100

    update

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -8,7 +8,7 @@ import { imgBase } from "../utils/requests";
 // import { AiOutlineEye } from "react-icons/ai";
 import Moment from "react-moment";
 import { buildStyles, CircularProgressbar } from "react-circular-progressbar";
-import millify from "millify";
+// import millify from "millify";
 import Loader from "./Loader";
 
 const MovieDetails = () => {
@@ -23,8 +23,10 @@ const MovieDetails = () => {
       const movieDetailRes = await axios(
         `${movieDetailUrl}/${id}?api_key=${API_KEY}&language=en-US&append_to_response=videos`
       );
-      setMovieDetail(movieDetailRes.data);
-      setLoading(false);
+      setTimeout(() => {
+        setMovieDetail(movieDetailRes.data);
+        setLoading(false);
+      }, 1000);
     } catch (err) {
       console.log(err.message);
     }
@@ -72,54 +74,72 @@ const MovieDetails = () => {
       <Header />
       <div className=" container px-4">
         <img
-          src={imgBase + movieDetail.backdrop_path}
+          src={imgBase + movieDetail.poster_path}
           alt=""
-          className="mt-4 mb-4 shadow-lg"
+          className="mt-4 mb-5 w-[17rem] rounded-lg shadow-lg"
         />
-        <div className="flex items-center gap-6 mb-8">
-          <div style={{ width: 50, height: 50 }} className="">
-            <CircularProgressbar
-              value={movieDetail.vote_average}
-              maxValue={10}
-              text={`${movieDetail.vote_average / 1}`}
-              strokeWidth={7}
-              background
-              backgroundPadding={6}
-              styles={buildStyles({
-                backgroundColor: "#000000",
-                textColor: "#fff",
-                pathColor: "#ff3030",
-                trailColor: "transparent",
-                textSize: "30px",
-              })}
-            />
-          </div>
-          {movieDetail.adult === true && <span>pg</span>}
-          <span>{time_convert(movieDetail.runtime)}</span>
-          <Moment date={dateToFormat} fromNow />
-        </div>
         <div className="">
-          {movieDetail.tagline && (
-            <div className="flex items-end gap-4 mb-9">
-              <span className="font-bold text-sm">Tagline: </span>
-              <span className="text-gray-400">{movieDetail.tagline}</span>
+          <div className="font-montBold text-2xl mt-6 mb-5">
+            {movieDetail.title || movieDetail.name}
+          </div>
+          <div className="flex items-center gap-6 mb-8">
+            <div style={{ width: 50, height: 50 }} className="">
+              <CircularProgressbar
+                value={movieDetail.vote_average}
+                maxValue={10}
+                text={`${movieDetail.vote_average / 1}`}
+                strokeWidth={7}
+                background
+                backgroundPadding={6}
+                styles={buildStyles({
+                  backgroundColor: "#000000",
+                  textColor: "#fff",
+                  pathColor: "#ff3030",
+                  trailColor: "transparent",
+                  textSize: "30px",
+                })}
+              />
             </div>
-          )}
-          {movieDetail.original_language && (
-            <div className="flex items-end gap-4 mb-9">
-              <span className="font-bold text-sm">Language: </span>
-              <span className="text-gray-400">{lang}</span>
-            </div>
-          )}
-          {movieDetail.genres && (
-            <div className="flex items-end gap-4 mb-9">
-              <span className="font-bold text-sm">Genres: </span>
-              {movieDetail.genres.map((genre) => (
-                <span className="text-gray-400">{genre.name},</span>
-              ))}
-            </div>
-          )}
-          {/* {movieDetail.budget !== 0 && (
+            {movieDetail.adult === true && <span>pg</span>}
+            <span>{time_convert(movieDetail.runtime)}</span>
+            <Moment date={dateToFormat} fromNow />
+          </div>
+          <div className="">
+            {movieDetail.tagline && (
+              <div className="flex items-end gap-4 mb-9">
+                <span className="font-bold text-sm">Tagline: </span>
+                <span className="text-gray-400">{movieDetail.tagline}</span>
+              </div>
+            )}
+            {movieDetail.adult && (
+              <div className="flex items-end gap-4 mb-9">
+                <span className="font-bold text-sm">PG: </span>
+                {movieDetail.adult === true ? (
+                  <span className="text-gray-400">True</span>
+                ) : (
+                  <span className="text-gray-400">False</span>
+                )}
+              </div>
+            )}
+            {movieDetail.original_language && (
+              <div className="flex items-end gap-4 mb-9">
+                <span className="font-bold text-sm">Language: </span>
+                <span className="text-gray-400">{lang}</span>
+              </div>
+            )}
+            {movieDetail.genres && (
+              <div className="flex flex-col gap-4 mb-7">
+                <span className="font-bold text-sm">Genres: </span>
+                <div className="flex flex-wrap gap-x-4 gap-y-3 items-end">
+                  {movieDetail.genres.map((genre) => (
+                    <span className="border md:border-2 border-red-900 shadow-lg px-5 py-3 rounded-full text-gray-400">
+                      {genre.name}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            )}
+            {/* {movieDetail.budget !== 0 && (
             <div className="flex items-end gap-4 mb-9">
               <span className="font-bold text-sm">Budget: </span>
               <span className="text-gray-400">
@@ -127,18 +147,29 @@ const MovieDetails = () => {
               </span>
             </div>
           )} */}
-          {movieDetail.production_companies && (
-            <div className="flex flex-col gap-4 mb-7">
-              <span className="font-bold text-sm">Production Companies: </span>
-              <div className="flex flex-wrap gap-x-4 gap-y-3 items-end">
-                {movieDetail.production_companies.map((pc) => (
-                  <span className="border md:border-2 border-red-900 shadow-lg px-5 py-3 rounded-full text-gray-400">
-                    {pc.name}
-                  </span>
-                ))}
+            {movieDetail.production_companies && (
+              <div className="flex flex-col gap-4 mb-7">
+                <span className="font-bold text-sm">
+                  Production Companies:{" "}
+                </span>
+                <div className="flex flex-wrap gap-x-4 gap-y-3 items-end">
+                  {movieDetail.production_companies.map((pc) => (
+                    <span className="border md:border-2 border-red-900 shadow-lg px-5 py-3 rounded-full text-gray-400">
+                      {pc.name}
+                    </span>
+                  ))}
+                </div>
               </div>
-            </div>
-          )}
+            )}
+            {movieDetail.overview && (
+              <div className="flex flex-col gap-4 mb-9">
+                <span className="font-bold text-lg block">Synopsis: </span>
+                <span className="text-gray-400 text-sm">
+                  {movieDetail.overview}
+                </span>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
