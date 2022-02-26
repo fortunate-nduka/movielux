632a691981e201d6bb3f3d04e2499dce04101c35
Fortunate ogodu <ogodujoseph@gmail.com>
2022-02-26 21:52:39 +0100

    update

--- src/components/CarouselThumb.jsx
+++ src/components/CarouselThumb.jsx
@@ -78,7 +78,7 @@ const CarouselThumb = ({
         className="w-full bg-cover bg-center bg-no-repeat min-h-screen md:h-full"
       >
         <Header />
-        <div className="container pb-20 md:mx-auto md:px-5 relative flex justify-center lg:justify-between items-center">
+        <div className="container pt-24 pb-20 md:mx-auto md:px-5 relative flex justify-center lg:justify-between items-center">
           <div className="absolute right-3 top-5">
             <button className="border border-white rounded-full p-2 mr-3 shadow-lg">
               <BsArrowLeft fontSize={15} onClick={handlePrev} />
@@ -87,7 +87,7 @@ const CarouselThumb = ({
               <BsArrowRight fontSize={15} onClick={handleNext} />
             </button>
           </div>
-          <div className="px-4 pt-24 lg:mt-0 mb-10 lg:basis-3/5">
+          <div className="px-4 mb-10 lg:basis-3/5">
             <Moment date={dateToFormat} fromNow className="text-gray-300" />
             <div className="font-poppins text-3xl md:text-4xl font-bold mt-4 md:mt-5 w-full">
               {title || name}
@@ -119,7 +119,6 @@ const CarouselThumb = ({
               <button className="tracking-wider px-7 py-4 border border-white rounded-full font-semibold flex items-center justify-center ">
                 Watch Trailer <AiOutlineEye className="ml-2 text-lg" />
               </button>
-              
             </div>
           </div>
           <div className="hidden lg:block lg:w-1/4">
