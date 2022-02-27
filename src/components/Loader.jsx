ec710c4cb20cf132bcaa20c5e538809c666ffa80
Fortunate ogodu <ogodujoseph@gmail.com>
2022-02-27 19:24:04 +0100

    update

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,20 +1,26 @@
 const Loader = () => {
   return (
-    <div className="bg-[rgba(0,0,0,.9)] h-screen w-full flex items-center justify-center text-center fixed top-0 left-0 right-0 bottom-0 z-30">
-      <h1 className="font-poppins font-black text-2xl md:text-4xl absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4">
-        <span className="letter letter-ani">L</span>
-        <span className="letter">O</span>
-        <span className="letter letter-3">A</span>
-        <span className="letter letter-4">D</span>
-        <span className="letter letter-4">I</span>
-        <span className="letter">N</span>
-        <span className="letter letter-ani">G</span>
-        <span className="letter letter-ani">.</span>
-        <span className="letter letter-ani">.</span>
-        <span className="letter letter-ani">.</span>
-      </h1>
-    </div>
+    <section>
+      <div className="loader-wrapper">
+        <div className="loader animate-spin before:animate-spin-reverse after:animate-spin"></div>
+        <div className="loader-section section-left"></div>
+        <div className="loader-section section-right"></div>
+      </div>
+    </section>
   );
 };
-
+// <div className="bg-[rgba(0,0,0,.9)] h-screen w-full flex items-center justify-center text-center fixed top-0 left-0 right-0 bottom-0 z-30">
+//   <h1 className="font-poppins font-black text-2xl md:text-4xl absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4">
+//     <span className="letter letter-ani">L</span>
+//     <span className="letter">O</span>
+//     <span className="letter letter-3">A</span>
+//     <span className="letter letter-4">D</span>
+//     <span className="letter letter-4">I</span>
+//     <span className="letter">N</span>
+//     <span className="letter letter-ani">G</span>
+//     <span className="letter letter-ani">.</span>
+//     <span className="letter letter-ani">.</span>
+//     <span className="letter letter-ani">.</span>
+//   </h1>
+// </div>
 export default Loader;
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,29 @@ module.exports = {
       colors: {
         black3: "rgba(0,0,0,.3)",
       },
+      keyframes: {
+        spin: {
+          "0%": {
+            transform: "rotate(0deg)",
+          },
+          "100%": {
+            transform: "rotate(360deg)",
+          },
+        },
+        "spin-reverse": {
+          "0%": {
+            transform: "rotate(0deg)",
+          },
+
+          "100%": {
+            transform: "rotate(-360deg)",
+          },
+        },
+      },
+      animation: {
+        spin: "spin 1s linear infinite",
+        "spin-reverse": "spin-reverse 0.6s linear infinite",
+      },
     },
   },
   plugins: [require("tailwind-scrollbar-hide")],
